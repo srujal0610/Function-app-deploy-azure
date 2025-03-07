0d7327664daf1239b012a1ed938efe114419ba79
Srujal Prajapati <sprajapati@surekhatech.com>
2025-03-07 17:00:40 +0530

    adding new files for node

--- api/index.js
+++ api/index.js
@@ -1,13 +1,7 @@
 module.exports = async function (context, req) {
-    context.log('JavaScript HTTP trigger function processed a request.');
-
-    const name = (req.query.name || (req.body && req.body.name));
-    const responseMessage = name
-        ? "Hello, " + name + ". This HTTP triggered function executed successfully and successfully."
-        : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
-
     context.res = {
-        // status: 200, /* Defaults to 200 */
-        body: responseMessage
+        status: 200,
+        body: { message: "Hello from Azure Function!" }
     };
-}
+};
+
